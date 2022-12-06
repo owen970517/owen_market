c11fe3833f5db80381745f2650941423a23d7fe9
owen970517 <tamia123123@naver.com>
2022-12-06 18:23:35 +0900

    기존 이미지 업로드 및 미리보기 코드를 useForm으로 개선

--- src/Components/products/Modify.tsx
+++ src/Components/products/Modify.tsx
@@ -4,77 +4,74 @@ import { db, storage } from "../../firebase";
 import styled from "styled-components";
 import { IData } from "../../type/ItemProps";
 import noImg from '../../ImgSrc/noimage.jpg'
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { IForm } from "../../type/InputForm";
 
 function Modify() {
     const [data, setData] = useState<IData>();
+    const [imagePreview, setImagePreview] = useState('');
+    const {register , handleSubmit,watch} = useForm<IForm>();
     const nav = useNavigate();
     const params = useParams();
+    const uploadImg = watch('image');
     const date = new Date();
     const years = String(date.getFullYear()).padStart(4,'0');
     const month = String(date.getMonth()+1).padStart(2,'0');
     const day = String(date.getDate()).padStart(2,'0');
-    const FileRef = useRef() as any;
+    const FileRef = useRef<HTMLInputElement | null>(null);
     useEffect(() => {
         const fetchData = async () => {
             const snapshot = await db.collection('Product').doc(params.uid).get()
             setData(snapshot.data());
         }
-        fetchData()
-    },[params.uid])
-    const {register , handleSubmit,watch} = useForm({
-        defaultValues : {
-            item : data?.상품명,
-            price : data?.가격
+        setImagePreview(data?.이미지!);
+        if(uploadImg && uploadImg.length > 0) {
+            const file = uploadImg[0]
+            setImagePreview(URL.createObjectURL(file))
         }
-    });
-    const fileRef = useRef<HTMLInputElement | null>();
-    const onFileChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-        if(e.target.files) {
-            const Img =e.target.files[0];
+        fetchData()
+    },[data?.이미지, params.uid, uploadImg])
+    const onModified:SubmitHandler<IForm> = async (props) => {
+        if(props.image[0]) {
+            const Img = props.image[0];
             const storageRef = storage.ref();
-            const ImgRef = storageRef.child(`image/${Img.name}`);
+            const ImgRef = storageRef.child(`user_image/${Img.name}`);
             const uploadImg = ImgRef.put(Img);
-            uploadImg.on('state_changed' ,
-            null ,  
+            uploadImg.on('state_changed', 
+            // 변화시 동작하는 함수 
+            null, 
+            //에러시 동작하는 함수
             (error) => {
-                console.log(error);
-            },      
+              console.error('실패사유는', error);
+            }, 
+            // 성공시 동작하는 함수
             async () => {
-                await uploadImg.snapshot.ref.getDownloadURL().then((url) => {
-                    setData({
-                        이미지 :url,
-                        올린사람 : data?.올린사람,
-                        상품명 : data?.상품명,
-                        가격 : data?.가격,
-                        날짜 : data?.날짜
-                    })
-                });
-              }
-            )
-        } 
-    }
-    const onModified = async () => {
-        await db.collection('Product').doc(params.uid).update({
-            이미지 : data?.이미지,
-            상품명 : watch('item'), 
-            가격 : watch('price'),
-            날짜 : `${years}년${month}월${day}일`
-        })
-        nav('/');
+              const url = await uploadImg.snapshot.ref.getDownloadURL()
+              await db.collection('Product').doc(params.uid).update({
+                이미지 : url,
+                상품명 : watch('item'), 
+                가격 : watch('price'),
+                날짜 : `${years}년${month}월${day}일`
+                })
+                nav('/');
+              });
+            }
     }
     return (
         <>
-            <FileInput onClick={() => FileRef.current.click()}>
-                <BgImg src={data?.이미지 ? data?.이미지 : noImg} width='30%' height='300px'></BgImg>
+            <FileInput onClick={() => FileRef.current?.click()}>
+                <BgImg src={imagePreview ? imagePreview : noImg} width='30%' height='300px'></BgImg>
                 <label>사진 변경</label>
-                <input type='file' onChange={onFileChange} style={{ display : 'none'}} ref={FileRef}/>
+                <input {...register('image')} type="file" ref={(data) => {
+                    register('image').ref(data);
+                    FileRef.current = data
+                }}></input>
             </FileInput>
             <div>
                 <h5>올린사람 : {data?.올린사람} </h5>
-                <h5>상품명 : <input type='text' {...register('item')} ></input></h5>
+                <h5>상품명 : <input type='text' {...register('item')} defaultValue={data?.상품명}></input></h5>
                 <p>올린날짜 : {data?.날짜}</p>
-                <p>가격 : <input type='text' {...register('price')} ></input></p>
+                <p>가격 : <input type='text' {...register('price')} defaultValue={data?.가격}></input></p>
             </div>
             <button onClick={handleSubmit(onModified)}>수정 완료</button>
         </>
@@ -102,6 +99,16 @@ const FileInput = styled.div`
     border-color: #2e6da4;
     border-bottom-color: #e2e2e2;
     border-radius: .25em;
+  }
+  input[type='file'] {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip:rect(0,0,0,0);
+    border: 0;
   }
 `
 export default Modify
