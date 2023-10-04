1451e8906c7332b818131ddcf0f3b237d92b5a2e
owen970517 <tamia123123@naver.com>
2023-10-04 18:22:19 +0900

    refactor: 경로 변수 상수화

--- src/Components/Layout/Pages.tsx
+++ src/Components/Layout/Pages.tsx
@@ -2,12 +2,13 @@ import { lazy} from "react"
 import { Route, Routes } from "react-router-dom"
 import Chat from "./Chat"
 import ChatRooms from "./ChatRooms"
+import { ROUTES } from "../../constants/routes"
 
 const Home = lazy(() =>import('../products/Home'))
 const Profile = lazy(() => import("../auth/Profile"))
 const AddProduct = lazy(() => import('../products/AddProduct'))
 const Detail = lazy(() => import('../products/Detail'))
-const Sign = lazy(() => import("../auth/Sign"))
+const Sign = lazy(() => import("../auth/AuthContainer"))
 const Modify = lazy(() => import("../products/Modify"))
 const Cart = lazy(() => import("../products/Cart"))
 const Search = lazy(() => import("../products/SearchedProduct"))
@@ -15,16 +16,16 @@ const Search = lazy(() => import("../products/SearchedProduct"))
 const Pages = () => {
     return ( 
         <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/write' element={<AddProduct />}></Route>
-            <Route path='/profile/' element={<Profile/>}></Route>
-            <Route path='/detail/:id' element={<Detail />}></Route>
-            <Route path='/sign' element={<Sign/>}></Route>
-            <Route path='/chatrooms' element={<ChatRooms/>} />
-            <Route path='/chat/:product' element={<Chat/>}></Route>
-            <Route path='/modify/:uid' element={<Modify/>}></Route>
-            <Route path='/cart' element={<Cart/>}/>
-            <Route path='/search/:input' element={<Search />}/>
+          <Route path={ROUTES.HOME} element={<Home/>} />
+          <Route path={ROUTES.WRITE} element={<AddProduct />} />
+          <Route path={ROUTES.PROFILE} element={<Profile/>} />
+          <Route path={ROUTES.DETAIL} element={<Detail />} />
+          <Route path={ROUTES.SIGN} element={<Sign/>} />
+          <Route path={ROUTES.CHATROOMS} element={<ChatRooms/>}/>
+          <Route path={ROUTES.CHAT_PRODUCT} element={<Chat/>}/>
+          <Route path={ROUTES.MODIFY_UID }element={<Modify/>}/>
+          <Route path={ROUTES.CART }element={<Cart />}/>
+          <Route path={ROUTES.SEARCH} element={<Search />}/>
         </Routes>
     )
 }
---
+++ src/constants/routes.ts
@@ -0,0 +1,12 @@
+export const ROUTES = {
+    HOME: '/',
+    WRITE: '/write',
+    PROFILE: '/profile/',
+    DETAIL: '/detail/:id',
+    SIGN: '/sign',
+    CHATROOMS: '/chatrooms',
+    CHAT_PRODUCT: '/chat/:product',
+    MODIFY_UID: '/modify/:uid',
+    CART:'/cart',
+    SEARCH:'/search/:input'
+}
