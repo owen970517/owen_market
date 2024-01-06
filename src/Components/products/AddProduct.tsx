a9aa58d0bff0d78e6621a546b3c68cf65ed3e88f
owen970517 <tamia123123@naver.com>
2024-01-06 17:29:09 +0900

    feat: 상품 등록 시 상품 설명을 넣을 수 있도록 추가

--- src/Components/products/AddProduct.tsx
+++ src/Components/products/AddProduct.tsx
@@ -26,11 +26,12 @@ const AddProduct = () => {
       setImgPreview(URL.createObjectURL(file));
     }
   },[imgSrc])
-  const onSubmit:SubmitHandler<IForm> = async (props) => {
+  const onAddProduct:SubmitHandler<IForm> = async (props) => {
     let url =''
     const Img = props.image[0];
     const date = dayjs();
     const formattedDate = date.format('YYYY년MM월DD일');
+    const description = props.description.replace(/\n/g, '<br>');
     if(Img) {
       const compressedImage = await compressImage(Img);
       url= await uploadImageToStorage(compressedImage);
@@ -43,26 +44,30 @@ const AddProduct = () => {
       상태 : '판매중',
       올린사람 : user.displayName,
       날짜 : formattedDate,
-      이미지 : url
+      이미지 : url,
+      설명 : description,
     });
     nav('/');
   }
   const onImgDel = async () => {
     setImgPreview("");
+    if (fileRef.current) {
+      fileRef.current.value = "";
+    }
   }
   return (
     <HelmetProvider>
       <Helmet>
-        <title>{`중고 등록 | 중고사이트`}</title>
+        <title>{`상품 등록 | 중고사이트`}</title>
       </Helmet>
       <Wrapper>
         {imgPreview && 
-          <div>
+          <PreviewWrapper>
             <Preview src={imgPreview } alt="없음"/>
-            <button onClick={onImgDel}>삭제</button>
-          </div>
+            <DeleteBtn onClick={onImgDel}>❌</DeleteBtn>
+          </PreviewWrapper>
         } 
-        <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form onSubmit={handleSubmit(onAddProduct)}>
             <FileInput onClick={() => {fileRef.current?.click()}}>
               <label>업로드</label>
               <input {...register('image')} type="file" ref={(data) => {
@@ -74,7 +79,8 @@ const AddProduct = () => {
             <Input {...register("region" , {required : true})} placeholder='지역'></Input>
             <Input {...register("item" , {required :true , maxLength:10})} placeholder="상품명"></Input>
             <Input {...register("price" , {required :true , maxLength:20})} placeholder="가격"></Input>
-            <Input type="submit" value='올리기' onClick={() => onSubmit}></Input>
+            <Textarea {...register("description" , {required :true , maxLength:200})} placeholder="상품 설명"></Textarea>
+            <Input type="submit" value='올리기' onClick={() => onAddProduct}></Input>
         </Form>
       </Wrapper>
     </HelmetProvider>
@@ -85,24 +91,56 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  background-color: #FFF5E1;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+  width: 100%;
+  max-width: 500px;
 `
 
 const Input = styled.input`
-width: 500px;
-height: 50px;
-  margin-bottom : 10px;
-  padding : 10px;
-  font-size: 20px;
+  width: 100%;
+  height: 50px;
+  border-radius: 5px;
+  border: 1px solid #FF8A3D;
+  padding: 10px;
+  font-size: 16px;
+  margin-bottom: 10px;
+  &:focus {
+    border-color: #FF8A3D;
+  }
 `
+const PreviewWrapper = styled.div`
+  position: relative;
+`
+const DeleteBtn = styled.div`
+  position: absolute;
+  top : -10px;
+  right: -10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: #000;
+  cursor : pointer;
+`
+
 const Preview = styled.img`
-  width : 300px;
-  height : 300px;
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+  border-radius: 10px;
+  margin-bottom: 20px;
 `
+
 const FileInput = styled.div`
   label {
     display: inline-block;
@@ -112,10 +150,9 @@ const FileInput = styled.div`
     line-height: normal;
     vertical-align: middle;
     cursor: pointer;
-    background-color: #337ab7;
-    border-color: #2e6da4;
-    border-bottom-color: #e2e2e2;
+    background-color: #FF8A3D;
     border-radius: .25em;
+    margin-bottom: 10px;
   }
   input[type='file'] {
     position: absolute;
@@ -126,6 +163,19 @@ const FileInput = styled.div`
     overflow: hidden;
     clip:rect(0,0,0,0);
     border: 0;
+  }
+`
+
+const Textarea = styled.textarea`
+  width: 100%;
+  height: 100px;
+  border-radius: 5px;
+  border: 1px solid #FF8A3D;
+  padding: 10px;
+  font-size: 16px;
+  margin-bottom: 10px;
+  &:focus {
+    border-color: #FF8A3D;
   }
 `
 export default AddProduct
--- src/type/InputForm.ts
+++ src/type/InputForm.ts
@@ -11,4 +11,5 @@ export interface IForm {
     nickname :string
     passwordConfirm :string
     chat : string
+    description : string
   }
