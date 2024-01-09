4710f6be5660be0ea5cd323eac7a3054aa8ae582
owen970517 <tamia123123@naver.com>
2024-01-09 17:36:24 +0900

    refactor: 변수명 변경 및 type 추가

--- src/Components/Layout/Region.tsx
+++ src/Components/Layout/Region.tsx
@@ -8,15 +8,15 @@ import { Regions } from "../../constants/region";
 
 const Region = () => {
     const dispatch = useDispatch()
-    const {activeRegion,data} = useSelector((state:RootState)=> state.region);
+    const {activeRegion,allProducts} = useSelector((state:RootState)=> state.region);
     useEffect(() => {
         if (activeRegion === '전체') {
-            dispatch(regionActions.setFilteredData(data));
+            dispatch(regionActions.setFilteredProducts(allProducts));
             return;
         } 
-        const filtered = data.filter((region:IData) => region.지역 === activeRegion)
-        dispatch(regionActions.setFilteredData(filtered));
-    },[data, dispatch, activeRegion]);
+        const filtered = allProducts.filter((region:IData) => region.지역 === activeRegion)
+        dispatch(regionActions.setFilteredProducts(filtered));
+    },[allProducts, dispatch, activeRegion]);
     const onRegionClick = (e:React.MouseEvent<HTMLButtonElement>) => {
         dispatch(regionActions.changeRegion(e.currentTarget.innerText))
     }
--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialCartState:any = {
-    cartItems :[]
+const initialCartState = {
+    cartItems :[] as string[]
 }
 
 const cartSlice = createSlice({
