1d13e9f1d560851ee3f88d663eae00b1da96877f
owen970517 <tamia123123@naver.com>
2024-01-14 16:45:32 +0900

    refactor: 이미지 압축 시 최대 너비 및 높이 변경

--- src/hooks/useCompressImage.tsx
+++ src/hooks/useCompressImage.tsx
@@ -2,12 +2,11 @@ import imageCompression from "browser-image-compression";
 
 export const useCompressImage = () => {
     const compressImage = async (image:File) => {
-        const options = {
-          maxSizeMB: 1,
-          maxWidthOrHeight: 1024,
-          useWebWorker: true
-        };
-        return await imageCompression(image , options);
+      const options = {
+        maxSizeMB: 1,
+        maxWidthOrHeight: 600,
+      };
+      return await imageCompression(image , options);
     };
 
     return {compressImage}
