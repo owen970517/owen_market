51f284b289f9a6fc82d1c9da8aa9be4919b545b1
owen970517 <tamia123123@naver.com>
2024-01-19 16:21:19 +0900

    refactor: 이미지 보여주는 코드 수정

--- src/Components/products/AllProducts.tsx
+++ src/Components/products/AllProducts.tsx
@@ -23,7 +23,7 @@ const AllProducts = () => {
         filteredProducts.length > 0 ? filteredProducts.map((p: IData, idx: number) => {
           return (
             <S.Item key={p.id}>
-              <LazyLoadImage src={p?.이미지?.length! > 0 ? p.이미지![0] : noImg} alt='이미지를 불러오지 못했습니다.' width={300} height={300} effect='blur' />
+              <LazyLoadImage src={p.이미지 ? p.이미지[0] : noImg} alt='이미지를 불러오지 못했습니다.' width={300} height={300} effect='blur' />
               <div ref={idx === filteredProducts.length - 1 ? ref : undefined}>
                 <S.StyledLink to={`/detail/${encodeURIComponent(p.id!)}`}><h2>{p.상품명}</h2></S.StyledLink>
                 <h3>{dayjs(p.날짜).fromNow()}</h3>
