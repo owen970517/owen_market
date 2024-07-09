f9d2bd56723a012e0fe266de30912e03271cb599
owen970517 <tamia123123@naver.com>
2024-07-09 22:50:26 +0900

    design: 768px에서 css 수정

--- src/Components/products/AllProducts.tsx
+++ src/Components/products/AllProducts.tsx
@@ -23,14 +23,14 @@ const AllProducts = () => {
         filteredProducts.length > 0 ? filteredProducts.map((p: IData, idx: number) => {
           return (
             <S.Item key={p.id}>
-              <LazyLoadImage src={p.이미지!.length > 0  ? p.이미지![0] : noImg} alt='이미지를 불러오지 못했습니다.' width={300} height={300} effect='blur' />
-              <div ref={idx === filteredProducts.length - 1 ? ref : undefined}>
+              <LazyLoadImage src={p.이미지!.length > 0  ? p.이미지![0] : noImg} alt='이미지를 불러오지 못했습니다.' effect='blur' />
+              <S.InfoWrapper ref={idx === filteredProducts.length - 1 ? ref : undefined}>
                 <S.StyledLink to={`/detail/${encodeURIComponent(p.id!)}`}><h2>{p.상품명}</h2></S.StyledLink>
                 <S.ItemInfo>
                   <p>{`${p.지역} ${p.세부지역} · ${dayjs(p.날짜).fromNow()}`}</p>
                 </S.ItemInfo>
                 <h3>{p.가격?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</h3>
-              </div>
+              </S.InfoWrapper>
             </S.Item>
           );
         }) : <h1>상품이 존재하지 않습니다.</h1>
--- src/styles/Products.styled.ts
+++ src/styles/Products.styled.ts
@@ -2,16 +2,24 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Grid = styled.div`
-  display : grid;
-  grid-template-columns : repeat(auto-fit, minmax(20rem,1fr));
-  grid-gap : 20px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 20px;
   place-items: center;
   padding: 20px;
-  background-color: #f8f8f8; 
+  background-color: #f8f8f8;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    grid-gap: 10px;
+    
+  }
 `;
 
 export const Item = styled.div`
-  display : flex;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -19,7 +27,7 @@ export const Item = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   padding: 10px;
   background-color: white;
-  transition: transform .2s;
+  transition: transform 0.2s;
 
   &:hover {
     transform: scale(1.03);
@@ -29,23 +37,65 @@ export const Item = styled.div`
     margin: 10px 0;
     text-align: center;
   }
+
+  img {
+    width: 300px;
+    height: 300px;
+  }
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    padding: 10px;
+    width: 90%;
+
+    h2,h3,p {
+      font-size: 14px;
+    }
+
+    img {
+      width : 100px;
+      height: 100px;
+    }
+  }
 `;
+
+export const InfoWrapper = styled.div`
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    align-items: baseline;
+  }
+`
+
 
 export const ItemInfo = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 10px;
+  }
+`;
 
 export const StyledLink = styled(Link)`
   display: flex;
   justify-content: center;
   text-decoration: none;
-  color : black;
+  color: black;
   font-weight: bold;
   text-transform: uppercase;
   letter-spacing: 1px;
+
   &:hover {
     color: #e67e22;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 0.9rem;
   }
 `;
